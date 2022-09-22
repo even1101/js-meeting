0475068b2310b1d3abf6cf1b701881163197cf8f
even.chang <even1101u@gmail.com>
2022-09-22 14:10:00 +0800

    webRtc method and js enum

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,31 @@
 import * as store from './store.js';
 import * as wss from './wss.js';
+import * as webRTCHandler from './webRTCHandler.js';
+import * as constants from './constants.js';
+
 
 const socket = io('/');
-
 wss.registerSocketEvent(socket);
+
+
+const personalCodeCopyButton = document.querySelector('#personal_code_copy_button');
+personalCodeCopyButton.addEventListener('click', () => {
+    const personalCode = store.getState().socketId;
+    navigator.clipboard && navigator.clipboard.writeText(personalCode);
+});
+
+
+const personalCodeChatButton = document.querySelector('#personal_code_chat_button');
+personalCodeChatButton.addEventListener('click', () => {
+    const calleePersonalCode = document.querySelector('#personal_code_input').value;
+    const callType = constants.callType.CHAT_PERSONAL_CODE;
+    webRTCHandler.sendPreOffer(callType, calleePersonalCode);
+});
+
+const personalCodeVideoButton = document.querySelector('#personal_code_video_button');
+personalCodeVideoButton.addEventListener('click', () => {
+    const calleePersonalCode = document.querySelector('#personal_code_input').value;
+    const callType = constants.callType.VIDEO_PERSONAL_CODE;
+    webRTCHandler.sendPreOffer(callType, calleePersonalCode);
+});
 
