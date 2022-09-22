0462416c250d4a35c0dfd2d4f735155a693841e7
even.chang <even1101u@gmail.com>
2022-09-22 10:31:27 +0800

    socket io server

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 3000;
 
 const app = express();
 const server = http.createServer(app);
-
+const io = require('socket.io')(server);
 
 app.use(express.static('public'));
 
@@ -13,8 +13,9 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/index.html');
 });
 
-app.get('/hello', (req, res) => {
-    res.send('hello');
+io.on('connection', (socket) => {
+    console.log('user connected to socket.io server.');
+    console.log(socket.id);
 });
 
 server.listen(PORT, () => {
