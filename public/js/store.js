deb507a706647b3764f777bda3971ba257fb19f3
even.chang <even1101u@gmail.com>
2022-09-22 13:25:41 +0800

    add store

---
+++ public/js/store.js
@@ -0,0 +1,55 @@
+const e = require("express");
+
+let state = {
+    socketId: null,
+    localStream: null,
+    remoteStream: null,
+    screenSharingStream: null,
+    allowConnectionsFromStrangers: false,
+    screenSharingActive: false,
+};
+
+
+export const setSocketId = (socketId) => {
+    state = {
+        ...state,
+        socketId
+    };
+};
+
+export const setLocalStream = (stream) => {
+    state = {
+        ...state,
+        localStream: stream
+    };
+};
+
+export const setAllowConnectionsFromStrangers = (allowConnection) => {
+    state = {
+        ...state,
+        allowConnectionsFromStrangers: allowConnection
+    };
+};
+
+export const setScreenSharingActive = (screenSharingActive) => {
+    state = {
+        ...state,
+        screenSharingActive: screenSharingActive
+    };
+};
+
+export const setScreenSharingStream = (stream) => {
+    state = {
+        ...state,
+        screenSharingStream: stream
+    };
+};
+
+export const setRemoteStream = (stream) => {
+    state = {
+        ...state,
+        remoteStream: stream
+    };
+};
+
+export const getState = () => state;
